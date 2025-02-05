b14e2a2d142cd517830b2eb24519d7800c924e3f
Edison Montes M. <edison.montes@gmail.com>
2025-02-05 14:02:33 -0300

    Update __init__.py

--- custom_components/daily_counter/__init__.py
+++ custom_components/daily_counter/__init__.py
@@ -1,1 +1,17 @@
+from .const import DOMAIN
 
+async def async_setup(hass, config):
+    """Set up the Daily Counter integration."""
+    return True
+
+async def async_setup_entry(hass, config_entry):
+    """Set up the Daily Counter integration from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    return True
