e00b74f89ff05259d8d04feb70d2ea2c883eda36
Edison Montes M. <edison.montes@gmail.com>
2025-02-06 17:02:24 -0300

    Update sensor.py

--- custom_components/daily_counter/sensor.py
+++ custom_components/daily_counter/sensor.py
@@ -1,81 +1,83 @@
 import logging
-from datetime import datetime
-from homeassistant.helpers.entity import Entity
-from homeassistant.util import dt as dt_util
-from homeassistant.helpers.event import async_track_state_change
+from datetime import datetime, timedelta
+import asyncio
+import voluptuous as vol
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.event import async_track_time_change, async_track_state_change
 from homeassistant.helpers.restore_state import RestoreEntity
-from .const import DOMAIN, CONF_NAME, CONF_SENSOR
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the Daily Counter sensor from a config entry."""
-    name = config_entry.data[CONF_NAME]
-    sensor_id = config_entry.data[CONF_SENSOR]  # Un solo sensor
-    unique_id = f"daily_counter_{name.lower().replace(' ', '_')}"
-    async_add_entities([DailyCounterSensor(hass, name, sensor_id, unique_id)])
+CONF_SENSORS = "sensors"
+CONF_TRIGGER_SENSOR = "trigger_sensor"
+CONF_NAME = "name"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_SENSORS): vol.All(
+            cv.ensure_list,
+            [
+                vol.Schema(
+                    {
+                        vol.Required(CONF_NAME): cv.string,
+                        vol.Required(CONF_TRIGGER_SENSOR): cv.entity_id,
+                    }
+                )
+            ],
+        )
+    }
+)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+    sensors = []
+    for sensor_config in entry.data[CONF_SENSORS]:
+        name = sensor_config[CONF_NAME]
+        trigger_sensor = sensor_config[CONF_TRIGGER_SENSOR]
+        sensor = DailyCounterSensor(hass, name, trigger_sensor)
+        sensors.append(sensor)
+    
+    async_add_entities(sensors, True)
 
 class DailyCounterSensor(RestoreEntity):
-    """Representation of a Daily Counter sensor."""
-
-    def __init__(self, hass, name, sensor_id, unique_id):
-        """Initialize the sensor."""
+    def __init__(self, hass: HomeAssistant, name: str, trigger_sensor: str):
         self._hass = hass
         self._name = name
-        self._sensor_id = sensor_id  # Un solo sensor
-        self._unique_id = unique_id
+        self._trigger_sensor = trigger_sensor
         self._state = 0
-        self._last_reset = dt_util.now().replace(hour=0, minute=0, second=0, microsecond=0)
-
-    @property
-    def unique_id(self):
-        """Return a unique ID for the sensor."""
-        return self._unique_id
+        self._reset_time = datetime.combine(datetime.today() + timedelta(days=1), datetime.min.time())
 
     async def async_added_to_hass(self):
-        """Restore state and set up listeners."""
-        await super().async_added_to_hass()
-        state = await self.async_get_last_state()
-        if state:
-            self._state = int(state.state)
-            self._last_reset = dt_util.parse_datetime(state.attributes.get("last_reset"))
+        last_state = await self.async_get_last_state()
+        if last_state and last_state.state.isdigit():
+            self._state = int(last_state.state)
+        
+        async_track_state_change(self._hass, self._trigger_sensor, self._increment_counter)
+        async_track_time_change(self._hass, self._reset_counter, hour=0, minute=0, second=0)
 
-        # Configurar listener para el sensor
-        async_track_state_change(self._hass, self._sensor_id, self._sensor_changed)
+    @callback
+    def _increment_counter(self, entity_id, old_state, new_state):
+        if old_state and new_state and old_state.state != new_state.state:
+            self._state += 1
+            self.async_write_ha_state()
+    
+    @callback
+    def _reset_counter(self, time):
+        self._state = 0
+        self.async_write_ha_state()
 
     @property
     def name(self):
-        """Return the name of the sensor."""
         return self._name
 
     @property
     def state(self):
-        """Return the state of the sensor."""
         return self._state
 
     @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes."""
-        return {
-            "last_reset": self._last_reset.isoformat(),
-            "sensor": self._sensor_id  # Mostrar el sensor en los atributos
-        }
-
-    async def _sensor_changed(self, entity_id, old_state, new_state):
-        """Handle sensor state changes."""
-        if new_state.state == "on":  # Cambia "on" por el estado que desees detectar
-            await self._increment_counter()
-
-    async def _increment_counter(self):
-        """Increment the counter."""
-        now = dt_util.now()
-        if now.date() != self._last_reset.date():
-            self._state = 0
-            self._last_reset = now.replace(hour=0, minute=0, second=0, microsecond=0)
-        self._state += 1
-        self.async_write_ha_state()
+    def unique_id(self):
+        return f"daily_counter_{self._name.lower().replace(' ', '_')}"
