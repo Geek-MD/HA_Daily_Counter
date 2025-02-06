6bc51c5f96e4901ed3978af6cb880652f6058ec5
Edison Montes M. <edison.montes@gmail.com>
2025-02-06 18:59:51 -0300

    Update __init__.py

--- custom_components/daily_counter/__init__.py
+++ custom_components/daily_counter/__init__.py
@@ -1,17 +1,1 @@
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-
-from .const import DOMAIN
-
-def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    hass.data.setdefault(DOMAIN, {})
-    return True
-
-def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-    return True
-
-def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.data[DOMAIN].pop(entry.entry_id, None)
-    return True
+# custom_components/custom_counter/__init__.py
