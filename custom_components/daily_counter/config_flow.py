be3448d2ab4270667ec7ce2e2d31fb07f8cb7908
Edison Montes M. <edison.montes@gmail.com>
2025-02-06 19:01:22 -0300

    Update config_flow.py

--- custom_components/daily_counter/config_flow.py
+++ custom_components/daily_counter/config_flow.py
@@ -1,42 +1,45 @@
+# custom_components/custom_counter/config_flow.py
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_registry import async_get as async_get_entity_registry
-from homeassistant.helpers.selector import selector
+from homeassistant.core import callback
+from homeassistant.helpers import selector
 
-from .const import DOMAIN, CONF_NAME, CONF_TRIGGER_SENSOR
+DATA_SCHEMA = vol.Schema({
+    vol.Required("name"): str,
+    vol.Required("sensor"): selector.EntitySelector(),
+})
 
-class DailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
+class CustomCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Manejador del flujo de configuración."""
 
     async def async_step_user(self, user_input=None):
-        errors = {}
-        hass: HomeAssistant = self.hass
-        entity_registry = async_get_entity_registry(hass)
-        
-        sensor_options = {
-            entity.entity_id: entity.original_name or entity.entity_id
-            for entity in entity_registry.entities.values()
-            if entity.entity_id.startswith("binary_sensor.") or entity.entity_id.startswith("sensor.")
-        }
-        
+        """Manejar el flujo de configuración inicial."""
         if user_input is not None:
-            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+            return self.async_create_entry(title=user_input["name"], data=user_input)
+
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Opciones de configuración."""
+        return CustomCounterOptionsFlow(config_entry)
+
+class CustomCounterOptionsFlow(config_entries.OptionsFlow):
+    """Manejador de opciones de configuración."""
+
+    def __init__(self, config_entry):
+        """Inicializar el flujo de opciones."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manejar las opciones."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_NAME): str,
-                    vol.Required(CONF_TRIGGER_SENSOR): selector({
-                        "select": {
-                            "options": [
-                                {"value": eid, "label": name} for eid, name in sensor_options.items()
-                            ],
-                            "mode": "dropdown"
-                        }
-                    })
-                }
-            ),
-            errors=errors,
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Required("sensor", default=self.config_entry.data["sensor"]): selector.EntitySelector(),
+            }),
         )
