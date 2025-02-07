13516aa3b2170de23c005bfc1ca52cd0dfa091ab
Edison Montes M. <edison.montes@gmail.com>
2025-02-06 23:05:15 -0300

    Update __init__.py

--- custom_components/daily_counter/__init__.py
+++ custom_components/daily_counter/__init__.py
@@ -1,1 +1,13 @@
-# custom_components/custom_counter/__init__.py
+# custom_components/daily_counter/__init__.py
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+DOMAIN = "daily_counter"
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Configurar una entrada de configuración."""
+    # Inicializar la plataforma "sensor"
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
