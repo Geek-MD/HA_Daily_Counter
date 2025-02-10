248fa09d2c38826e616bf86298ac1188779a083d
Edison Montes M. <edison.montes@gmail.com>
2025-02-10 13:30:58 -0300

    Update __init__.py

--- custom_components/daily_counter/__init__.py
+++ custom_components/daily_counter/__init__.py
@@ -10,3 +10,10 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Daily Counter integration."""
     _LOGGER.info("Daily Counter está siendo configurado")
     return True
+
+async def async_setup_entry(hass, config_entry):
+    """Set up the integration from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+    return True
