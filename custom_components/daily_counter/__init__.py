13ea10647ccd309ccc4238fc0d9b9e0cafa8cadc
Edison Montes M. <edison.montes@gmail.com>
2025-02-10 13:28:04 -0300

    Update __init__.py

--- custom_components/daily_counter/__init__.py
+++ custom_components/daily_counter/__init__.py
@@ -1,32 +1,12 @@
-import voluptuous as vol
-from homeassistant import config_entries
-from homeassistant.core import callback
-from homeassistant.helpers import selector
+import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN
+_LOGGER = logging.getLogger(__name__)
 
-class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for HA Daily Counter."""
+DOMAIN = "daily_counter"
 
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        errors = {}
-
-        if user_input is not None:
-            # Validar la entrada del usuario
-            sensor_entity = user_input["sensor_entity"]
-            if sensor_entity:
-                return self.async_create_entry(title="Daily Counter", data=user_input)
-            else:
-                errors["base"] = "sensor_required"
-
-        # Mostrar el formulario de configuración
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("sensor_entity"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor")
-                )
-            }),
-            errors=errors,
-        )
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Daily Counter integration."""
+    _LOGGER.info("Daily Counter está siendo configurado")
+    return True
