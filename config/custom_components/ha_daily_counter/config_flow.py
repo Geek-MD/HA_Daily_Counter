0bd440a5af1d6073f0391a244ca68b37cd29e274
Edison Montes M. <edison.montes@gmail.com>
2025-02-12 19:15:25 -0300

    Update config_flow.py

--- config/custom_components/ha_daily_counter/config_flow.py
+++ config/custom_components/ha_daily_counter/config_flow.py
@@ -8,11 +8,14 @@ class CounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Paso de configuración inicial."""
         if user_input is not None:
-            return self.async_create_entry(title="HA Daily Counter", data=user_input)
+            return self.async_create_entry(title=user_input["name"], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("counters", default=[]): vol.All(vol.Length(min=1), [str])
-            })
+                vol.Required("name", default="Contador Diario"): str,
+                vol.Required("event_type", default="state_changed"): str,
+                vol.Required("entity_id", default=""): str
+            }),
+            errors={}
         )
