15478a8703a411c01b69a48ef5a8afda2fbd0a8f
Edison Montes M. <edison.montes@gmail.com>
2025-02-14 17:04:57 -0300

    Create services.py

---
+++ custom_components/ha_daily_counter/services.py
@@ -0,0 +1,7 @@
+increment:
+  name: Increment Counter
+  description: Increase the specified counter by 1.
+  fields:
+    name:
+      name: Counter Name
+      description: The name of the counter to increment.
