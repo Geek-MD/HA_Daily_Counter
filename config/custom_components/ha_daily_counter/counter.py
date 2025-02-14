3e1cb2b8aeebc52c5b78a603d1695ef6d9922eb1
Edison Montes M. <edison.montes@gmail.com>
2025-02-14 16:47:26 -0300

    Update counter.py

--- config/custom_components/ha_daily_counter/counter.py
+++ config/custom_components/ha_daily_counter/counter.py
@@ -1,6 +1,5 @@
 class DailyCounter:
     def __init__(self, hass, name, initial_value=0):
-        """Inicializa un contador diario."""
         self.hass = hass
         self.name = name
         self._value = initial_value
@@ -10,9 +9,7 @@ class DailyCounter:
         return self._value
 
     def increment(self):
-        """Incrementa el contador."""
         self._value += 1
 
     def reset(self):
-        """Resetea el contador a 0."""
         self._value = 0
