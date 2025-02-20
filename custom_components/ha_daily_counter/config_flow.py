3139b00526ec0114291ab7e8c991df9f97324231
Edison Montes M. <edison.montes@gmail.com>
2025-02-20 18:38:11 -0300

    Update config_flow.py

--- custom_components/ha_daily_counter/config_flow.py
+++ custom_components/ha_daily_counter/config_flow.py
@@ -1,15 +1,21 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.helpers.device_registry import async_get as get_device_registry
 from .const import DOMAIN
+
 class CounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title=user_input["name"], data=user_input)
+
+        device_registry = await get_device_registry(self.hass)
+        devices = {device.id: device.name or device.identifiers for device in device_registry.devices.values()}
+
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("name", default="Daily Counter"): str,
-                vol.Required("event_type", default="state_changed"): str,
-                vol.Required("entity_id", default=""): str
+                vol.Required("device_id"): vol.In(devices),
+                vol.Required("event_type", default="state_changed"): str
             })
         )
