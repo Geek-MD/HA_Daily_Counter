6417ce009c95fa6c2618b243fe9ecbd80c2b4316
Edison Montes M. <edison.montes@gmail.com>
2025-05-10 21:46:45 -0400

    Create config_flow.py

---
+++ custom_components/ha_daily_counter/config_flow.py
@@ -0,0 +1,35 @@
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+import voluptuous as vol
+from homeassistant.helpers.selector import (
+    selector,
+)
+
+from .const import (
+    DOMAIN,
+    ATTR_TRIGGER_ENTITY,
+    ATTR_TRIGGER_STATE,
+    DEFAULT_NAME,
+)
+
+class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for HA Daily Counter."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
+                vol.Required(ATTR_TRIGGER_ENTITY): selector({
+                    "entity": {
+                        "multiple": False
+                    }
+                }),
+                vol.Required(ATTR_TRIGGER_STATE): str,
+            })
+        )
