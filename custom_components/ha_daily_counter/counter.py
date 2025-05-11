2ee886e47f61b0f4db66decfe89e67689327784f
Edison Montes M. <edison.montes@gmail.com>
2025-05-10 21:35:43 -0400

    Delete custom_components directory

--- custom_components/ha_daily_counter/counter.py
+++
@@ -1,9 +0,0 @@
-class DailyCounter:
-    def __init__(self, hass, name, initial_value=0):
-        self.hass = hass
-        self.name = name
-        self._value = initial_value
-    def increment(self):
-        self._value += 1
-    def reset(self):
-        self._value = 0
--- custom_components/ha_daily_counter/services.py
+++
@@ -1,7 +0,0 @@
-increment:
-  name: Increment Counter
-  description: Increase the specified counter by 1.
-  fields:
-    name:
-      name: Counter Name
-      description: The name of the counter to increment.
