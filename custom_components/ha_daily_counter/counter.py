69ec57fc5c0c7a6344839e8ab88e0c50eb28433d
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 17:18:13 -0400

    Update counter.py

--- custom_components/ha_daily_counter/counter.py
+++ custom_components/ha_daily_counter/counter.py
@@ -1,47 +1,49 @@
 import logging
-from homeassistant.core import callback
+
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import STATE_UNKNOWN
 from homeassistant.helpers.event import async_track_state_change, async_track_time_change
 from homeassistant.helpers.restore_state import RestoreEntity
-from homeassistant.const import STATE_UNKNOWN
+from homeassistant.core import callback
 
 _LOGGER = logging.getLogger(__name__)
 
-class HADailyCounterEntity(RestoreEntity):
-    """Representation of a HA Daily Counter entity."""
 
-    def __init__(self, name, trigger_entity, trigger_state):
+class HADailyCounterEntity(SensorEntity, RestoreEntity):
+    def __init__(self, name, trigger_entity, trigger_state, entry_id):
         self._attr_name = name
+        self._attr_icon = "mdi:counter"
+        self._attr_should_poll = False
+        self._attr_unique_id = f"{entry_id}_{trigger_entity.replace('.', '_')}"
+        self._state = 0
         self._trigger_entity = trigger_entity
         self._trigger_state = trigger_state
-        self._attr_should_poll = False
-        self._state = 0
 
     async def async_added_to_hass(self):
         last_state = await self.async_get_last_state()
         if last_state and last_state.state != STATE_UNKNOWN:
-            self._state = int(last_state.state)
+            try:
+                self._state = int(last_state.state)
+            except ValueError:
+                self._state = 0
+            self.async_write_ha_state()
 
         async_track_state_change(
-            self.hass, self._trigger_entity, self._handle_trigger
+            self.hass, self._trigger_entity, self._handle_trigger_change
         )
 
         async_track_time_change(
             self.hass, self._reset_counter, hour=0, minute=0, second=0
         )
 
-        self.async_write_ha_state()
-
     @callback
-    def _handle_trigger(self, entity_id, old_state, new_state):
-        if new_state is None:
-            return
-
-        if new_state.state == self._trigger_state:
+    def _handle_trigger_change(self, entity_id, old_state, new_state):
+        if new_state and new_state.state == self._trigger_state:
             self._state += 1
             self.async_write_ha_state()
 
     @callback
-    def _reset_counter(self, _now):
+    def _reset_counter(self, now=None):
         self._state = 0
         self.async_write_ha_state()
 
