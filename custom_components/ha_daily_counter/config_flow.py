56cb6713cab08f969e95b43ec87d72314dbe27f3
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 15:34:57 -0400

    Update config_flow.py

--- custom_components/ha_daily_counter/config_flow.py
+++ custom_components/ha_daily_counter/config_flow.py
@@ -2,13 +2,10 @@ from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
 import voluptuous as vol
 from homeassistant.helpers.selector import selector
-from homeassistant.core import callback
 
-from .const import DOMAIN, ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE, DEFAULT_NAME
-from .options_flow import HADailyCounterOptionsFlowHandler
+from .const import DOMAIN, ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE
 
-
-class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # type: ignore[call-arg]
+class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for HA Daily Counter."""
 
     VERSION = 1
@@ -16,21 +13,22 @@ class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # typ
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+            return self.async_create_entry(
+                title=user_input[CONF_NAME],
+                data={
+                    CONF_NAME: user_input[CONF_NAME],
+                    ATTR_TRIGGER_ENTITY: user_input[ATTR_TRIGGER_ENTITY],
+                    ATTR_TRIGGER_STATE: user_input[ATTR_TRIGGER_STATE],
+                }
+            )
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
+                vol.Required(CONF_NAME): str,
                 vol.Required(ATTR_TRIGGER_ENTITY): selector({
                     "entity": {"multiple": False}
                 }),
                 vol.Required(ATTR_TRIGGER_STATE): str,
             })
         )
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        """Return the options flow handler."""
-        return HADailyCounterOptionsFlowHandler(config_entry)
