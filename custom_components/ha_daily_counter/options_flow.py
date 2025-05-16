5c86e84139d1521d7dc88c4a2facbb46193a4711
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 17:24:00 -0400

    Update options_flow.py

--- custom_components/ha_daily_counter/options_flow.py
+++ custom_components/ha_daily_counter/options_flow.py
@@ -1,10 +1,10 @@
-from homeassistant import config_entries
-from homeassistant.const import CONF_NAME
-from homeassistant.helpers.selector import selector
+from __future__ import annotations
+
 import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.helpers.selector import selector
 
-from .const import ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE, DEFAULT_NAME
-
+from .const import ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE
 
 class HADailyCounterOptionsFlow(config_entries.OptionsFlow):
     """Options flow for HA Daily Counter."""
@@ -14,20 +14,58 @@ class HADailyCounterOptionsFlow(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
-            return await self._process_user_input(user_input)
+            return await self.async_step_manage(user_input)
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Required(CONF_NAME, default=self.config_entry.data.get(CONF_NAME, DEFAULT_NAME)): str,
-                vol.Required(ATTR_TRIGGER_ENTITY, default=self.config_entry.data.get(ATTR_TRIGGER_ENTITY)): selector({
-                    "entity": {"multiple": False}
-                }),
-                vol.Required(ATTR_TRIGGER_STATE, default=self.config_entry.data.get(ATTR_TRIGGER_STATE)): str,
+                vol.Required("action", default="add"): vol.In(["add", "remove"]),
             }),
         )
 
-    async def _process_user_input(self, user_input):
-        updated_options = self.config_entry.options.copy()
-        updated_options.update(user_input)
-        return self.async_create_entry(title="", data=updated_options)
+    async def async_step_manage(self, user_input):
+        action = user_input["action"]
+
+        if action == "add":
+            return await self.async_step_add()
+        if action == "remove":
+            return await self.async_step_remove()
+
+        return self.async_abort(reason="unknown_action")
+
+    async def async_step_add(self, user_input=None):
+        if user_input is not None:
+            counters = self.config_entry.options.get("counters", [])
+            counters.append({
+                "name": user_input["name"],
+                "trigger_entity": user_input["trigger_entity"],
+                "trigger_state": user_input["trigger_state"],
+            })
+
+            return self.async_create_entry(title="", data={"counters": counters})
+
+        return self.async_show_form(
+            step_id="add",
+            data_schema=vol.Schema({
+                vol.Required("name"): str,
+                vol.Required("trigger_entity"): selector({"entity": {}}),
+                vol.Required("trigger_state"): str,
+            }),
+        )
+
+    async def async_step_remove(self, user_input=None):
+        counters = self.config_entry.options.get("counters", [])
+
+        if user_input is not None:
+            counters = [c for c in counters if c["name"] != user_input["name"]]
+            return self.async_create_entry(title="", data={"counters": counters})
+
+        if not counters:
+            return self.async_abort(reason="no_counters_to_remove")
+
+        return self.async_show_form(
+            step_id="remove",
+            data_schema=vol.Schema({
+                vol.Required("name"): vol.In([c["name"] for c in counters]),
+            }),
+        )
