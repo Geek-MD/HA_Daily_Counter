6e4a79a3dff05fa4552d0a4a5296f55e2d5bc8cc
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 16:37:10 -0400

    Update config_flow.py

--- custom_components/ha_daily_counter/config_flow.py
+++ custom_components/ha_daily_counter/config_flow.py
@@ -1,34 +1,11 @@
 from homeassistant import config_entries
-from homeassistant.const import CONF_NAME
-import voluptuous as vol
-from homeassistant.helpers.selector import selector
-
-from .const import DOMAIN, ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE
+from .const import DOMAIN
 
 class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for HA Daily Counter."""
+    """Config flow for HA Daily Counter."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        if user_input is not None:
-            return self.async_create_entry(
-                title=user_input[CONF_NAME],
-                data={
-                    CONF_NAME: user_input[CONF_NAME],
-                    ATTR_TRIGGER_ENTITY: user_input[ATTR_TRIGGER_ENTITY],
-                    ATTR_TRIGGER_STATE: user_input[ATTR_TRIGGER_STATE],
-                }
-            )
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_NAME): str,
-                vol.Required(ATTR_TRIGGER_ENTITY): selector({
-                    "entity": {"multiple": False}
-                }),
-                vol.Required(ATTR_TRIGGER_STATE): str,
-            })
-        )
+        """Initial step: just create entry."""
+        return self.async_create_entry(title="HA Daily Counter", data={})
