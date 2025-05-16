c24f423810963c54dcd93ebe26438f6501d08a16
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 17:12:37 -0400

    Update const.py

--- custom_components/ha_daily_counter/const.py
+++ custom_components/ha_daily_counter/const.py
@@ -1,4 +1,9 @@
+"""Constants for HA Daily Counter integration."""
+
 DOMAIN = "ha_daily_counter"
-DEFAULT_NAME = "Daily Counter"
+
+CONF_NAME = "name"
 ATTR_TRIGGER_ENTITY = "trigger_entity"
 ATTR_TRIGGER_STATE = "trigger_state"
+
+DEFAULT_NAME = "Daily Counter"
