4a92cd9221689448b7cca831a7e9d25fa843bc8b
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 16:48:56 -0400

    Update counter.py

--- custom_components/ha_daily_counter/counter.py
+++ custom_components/ha_daily_counter/counter.py
@@ -1,45 +1,45 @@
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import STATE_UNKNOWN
-from homeassistant.helpers.event import async_track_state_change, async_track_time_change
 from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.core import callback
+from homeassistant.const import STATE_UNKNOWN
+from homeassistant.helpers.event import async_track_state_change, async_track_time_change
+import logging
 
-class HADailyCounter(SensorEntity, RestoreEntity):
-    def __init__(self, hass, entry, idx, cfg):
-        """Initialize."""
-        self.hass = hass
-        self._entry = entry
-        self._idx = idx
+_LOGGER = logging.getLogger(__name__)
 
-        self._attr_name = cfg.get("name")
-        self._trigger_entity = cfg.get("trigger_entity")
-        self._trigger_state = cfg.get("trigger_state")
+class HADailyCounterEntity(RestoreEntity):
+    """Representation of a Daily Counter entity."""
 
-        self._attr_unique_id = f"{entry.entry_id}_{idx}"
-        self._state = 0
+    def __init__(self, name: str, trigger_entity: str, trigger_state: str):
+        self._attr_name = name
         self._attr_icon = "mdi:counter"
         self._attr_should_poll = False
-        self._attr_native_unit_of_measurement = None
+        self._attr_native_value = 0
+
+        self._trigger_entity = trigger_entity
+        self._trigger_state = trigger_state
 
     async def async_added_to_hass(self):
         last_state = await self.async_get_last_state()
         if last_state and last_state.state != STATE_UNKNOWN:
-            self._state = int(last_state.state)
+            self._attr_native_value = int(last_state.state)
 
-        async_track_state_change(self.hass, self._trigger_entity, self._handle_trigger_change)
-        async_track_time_change(self.hass, self._reset_counter, hour=0, minute=0, second=0)
+        async_track_state_change(
+            self.hass, self._trigger_entity, self._handle_trigger
+        )
+
+        async_track_time_change(
+            self.hass, self._reset_counter, hour=0, minute=0, second=0
+        )
+
+        self.async_write_ha_state()
 
     @callback
-    def _handle_trigger_change(self, entity_id, old_state, new_state):
+    def _handle_trigger(self, entity_id, old_state, new_state):
         if new_state and new_state.state == self._trigger_state:
-            self._state += 1
+            self._attr_native_value += 1
             self.async_write_ha_state()
 
     @callback
-    def _reset_counter(self, now=None):
-        self._state = 0
+    def _reset_counter(self, now):
+        self._attr_native_value = 0
         self.async_write_ha_state()
-
-    @property
-    def native_value(self):
-        return self._state
