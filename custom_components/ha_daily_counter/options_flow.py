02672f1eadecbdf7078cc2a1518d9bc635962626
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 17:30:16 -0400

    Update options_flow.py

--- custom_components/ha_daily_counter/options_flow.py
+++ custom_components/ha_daily_counter/options_flow.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
 
-import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.helpers.selector import selector
-
-from .const import ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE
 
 class HADailyCounterOptionsFlow(config_entries.OptionsFlow):
     """Options flow for HA Daily Counter."""
@@ -14,58 +11,50 @@ class HADailyCounterOptionsFlow(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
-            return await self.async_step_manage(user_input)
+            action = user_input["action"]
+            if action == "add":
+                return await self.async_step_add_counter()
+            if action == "remove":
+                return await self.async_step_remove_counter()
 
         return self.async_show_form(
             step_id="init",
-            data_schema=vol.Schema({
-                vol.Required("action", default="add"): vol.In(["add", "remove"]),
-            }),
+            data_schema={
+                "action": selector({
+                    "select": {
+                        "options": ["add", "remove"]
+                    }
+                })
+            }
         )
 
-    async def async_step_manage(self, user_input):
-        action = user_input["action"]
-
-        if action == "add":
-            return await self.async_step_add()
-        if action == "remove":
-            return await self.async_step_remove()
-
-        return self.async_abort(reason="unknown_action")
-
-    async def async_step_add(self, user_input=None):
+    async def async_step_add_counter(self, user_input=None):
         if user_input is not None:
             counters = self.config_entry.options.get("counters", [])
-            counters.append({
-                "name": user_input["name"],
-                "trigger_entity": user_input["trigger_entity"],
-                "trigger_state": user_input["trigger_state"],
-            })
-
-            return self.async_create_entry(title="", data={"counters": counters})
+            counters.append(user_input)
+            return self.async_create_entry(title="Counter Added", data={"counters": counters})
 
         return self.async_show_form(
-            step_id="add",
-            data_schema=vol.Schema({
-                vol.Required("name"): str,
-                vol.Required("trigger_entity"): selector({"entity": {}}),
-                vol.Required("trigger_state"): str,
-            }),
+            step_id="add_counter",
+            data_schema={
+                "name": selector({"text": {}}),
+                "trigger_entity": selector({"entity": {"domain": "binary_sensor"}}),
+                "trigger_state": selector({"text": {}})
+            }
         )
 
-    async def async_step_remove(self, user_input=None):
-        counters = self.config_entry.options.get("counters", [])
-
+    async def async_step_remove_counter(self, user_input=None):
         if user_input is not None:
-            counters = [c for c in counters if c["name"] != user_input["name"]]
-            return self.async_create_entry(title="", data={"counters": counters})
+            counters = self.config_entry.options.get("counters", [])
+            counters = [c for c in counters if c.get("name") != user_input["name"]]
+            return self.async_create_entry(title="Counter Removed", data={"counters": counters})
 
-        if not counters:
-            return self.async_abort(reason="no_counters_to_remove")
+        counters = self.config_entry.options.get("counters", [])
+        options = [c["name"] for c in counters]
 
         return self.async_show_form(
-            step_id="remove",
-            data_schema=vol.Schema({
-                vol.Required("name"): vol.In([c["name"] for c in counters]),
-            }),
+            step_id="remove_counter",
+            data_schema={
+                "name": selector({"select": {"options": options}})
+            }
         )
