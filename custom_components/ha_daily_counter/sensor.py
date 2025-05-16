913744517e254cc6fb051d8b394be7e05dc92645
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 17:40:51 -0400

    Update sensor.py

--- custom_components/ha_daily_counter/sensor.py
+++ custom_components/ha_daily_counter/sensor.py
@@ -1,5 +1,5 @@
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .counter import HADailyCounterEntity
