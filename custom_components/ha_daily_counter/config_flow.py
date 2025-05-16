268bdd3c2763cc3e45f3ab23835c53da6668a6ba
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 17:01:36 -0400

    Update config_flow.py

--- custom_components/ha_daily_counter/config_flow.py
+++ custom_components/ha_daily_counter/config_flow.py
@@ -1,25 +1,23 @@
 from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
 import voluptuous as vol
 from homeassistant.helpers.selector import selector
 
-from .const import ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE, DEFAULT_NAME, DOMAIN
-from .options_flow import HADailyCounterOptionsFlow
+from .const import DOMAIN, ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE
 
 class HADailyCounterConfigFlow(config_entries.ConfigFlow):
     """Handle a config flow for HA Daily Counter."""
 
     VERSION = 1
-    DOMAIN = DOMAIN
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title=user_input["name"], data=user_input)
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("name", default=DEFAULT_NAME): str,
+                vol.Required(CONF_NAME): str,
                 vol.Required(ATTR_TRIGGER_ENTITY): selector({
                     "entity": {"multiple": False}
                 }),
@@ -27,6 +25,6 @@ class HADailyCounterConfigFlow(config_entries.ConfigFlow):
             })
         )
 
-    @staticmethod
-    def async_get_options_flow(config_entry):
-        return HADailyCounterOptionsFlow(config_entry)
+async def async_get_options_flow(config_entry):
+    from .options_flow import HADailyCounterOptionsFlow
+    return HADailyCounterOptionsFlow(config_entry)
