f51294b2fdc9efba43898d1dab63958db0b7fd49
Edison Montes M. <edison.montes@gmail.com>
2025-05-16 17:17:09 -0400

    Update config_flow.py

--- custom_components/ha_daily_counter/config_flow.py
+++ custom_components/ha_daily_counter/config_flow.py
@@ -1,23 +1,31 @@
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
-import voluptuous as vol
 from homeassistant.helpers.selector import selector
+import voluptuous as vol
 
-from .const import DOMAIN, CONF_NAME, ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE, DEFAULT_NAME
+from .const import DOMAIN, ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE, DEFAULT_NAME
+from .options_flow import HADailyCounterOptionsFlow
+
 
 class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for HA Daily Counter."""
+    """Handle a config flow for HA Daily Counter."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
         if user_input is not None:
             return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
-        schema = vol.Schema({
-            vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
-            vol.Required(ATTR_TRIGGER_ENTITY): selector({"entity": {"multiple": False}}),
-            vol.Required(ATTR_TRIGGER_STATE): str,
-        })
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
+                vol.Required(ATTR_TRIGGER_ENTITY): selector({"entity": {"multiple": False}}),
+                vol.Required(ATTR_TRIGGER_STATE): str,
+            }),
+        )
 
-        return self.async_show_form(step_id="user", data_schema=schema)
+
+async def async_get_options_flow(config_entry):
+    return HADailyCounterOptionsFlow(config_entry)
