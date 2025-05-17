ccfc87018792a43428149a871147f54fd413cad4
Edison Montes M. <edison.montes@gmail.com>
2025-05-17 19:34:02 -0400

    Update config_flow.py

--- custom_components/ha_daily_counter/config_flow.py
+++ custom_components/ha_daily_counter/config_flow.py
@@ -3,18 +3,19 @@ from homeassistant.config_entries import ConfigEntry
 from typing import Optional, Dict, Any
 
 from .options_flow import HADailyCounterOptionsFlow
+from .const import DOMAIN
 
 
-class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain="ha_daily_counter"):
-    """Configuration flow for the HA Daily Counter integration."""
+class HADailyCounterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for HA Daily Counter."""
 
     VERSION = 1
 
-    async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
-        """Handle the initial step of the user configuration flow."""
+    async def async_step_user(
+        self, user_input: Optional[Dict[str, Any]] = None
+    ) -> config_entries.FlowResult:
         return self.async_create_entry(title="HA Daily Counter", data={})
 
     @staticmethod
     def async_get_options_flow(config_entry: ConfigEntry) -> HADailyCounterOptionsFlow:
-        """Return the options flow handler for the config entry."""
         return HADailyCounterOptionsFlow(config_entry)
