652d4d6668bd396c2ad6ca892b3acf11a6ffd152
Edison Montes M. <edison.montes@gmail.com>
2025-05-17 19:33:15 -0400

    Update __init__.py

--- custom_components/ha_daily_counter/__init__.py
+++ custom_components/ha_daily_counter/__init__.py
@@ -1,20 +1,31 @@
+"""HA Daily Counter integration init."""
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.typing import ConfigType
 
-DOMAIN = "ha_daily_counter"
+from .const import DOMAIN
 
-# Explicitly define that this integration uses Config Entries only
+
+# Enforce that this integration only works via Config Entries
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
-async def async_setup(hass: HomeAssistant, config: dict):
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the HA Daily Counter integration from YAML (does nothing)."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up HA Daily Counter from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a HA Daily Counter config entry."""
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
