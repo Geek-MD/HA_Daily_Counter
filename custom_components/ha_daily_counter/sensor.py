f106b6649da3046f067356448c0d79e6a202ca62
Edison Montes M. <edison.montes@gmail.com>
2025-05-18 12:08:06 -0400

    Update sensor.py

--- custom_components/ha_daily_counter/sensor.py
+++ custom_components/ha_daily_counter/sensor.py
@@ -4,13 +4,13 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .counter import HADailyCounterEntity
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback
+) -> None:
     """Set up HA Daily Counter sensors from config entry."""
-    entities = []
     counters = entry.options.get("counters", [])
-
-    for counter in counters:
-        entities.append(HADailyCounterEntity(entry.entry_id, counter))
-
-    if entities:
-        async_add_entities(entities)
+    entities = [HADailyCounterEntity(hass, entry.entry_id, counter) for counter in counters]
+    async_add_entities(entities)
