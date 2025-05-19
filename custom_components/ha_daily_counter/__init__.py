ee66adf1dfcb0e3b57c9cb58d35f1a811c7cad20
Edison Montes M. <edison.montes@gmail.com>
2025-05-18 23:28:28 -0400

    Update __init__.py

--- custom_components/ha_daily_counter/__init__.py
+++ custom_components/ha_daily_counter/__init__.py
@@ -48,4 +48,5 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    result: bool = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return result
