323770a1f7d27e8c7c2de0d9512c7a987fc1850c
Edison Montes M. <edison.montes@gmail.com>
2025-05-19 14:01:49 -0400

    Update counter.py

--- custom_components/ha_daily_counter/counter.py
+++ custom_components/ha_daily_counter/counter.py
@@ -2,12 +2,13 @@ import logging
 from datetime import datetime, timedelta
 
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.core import callback, HomeAssistant
-from homeassistant.helpers.event import async_track_state_change, async_track_point_in_utc_time
+from homeassistant.core import HomeAssistant, callback, State
+from homeassistant.helpers.event import (
+    async_track_state_change,
+    async_track_point_in_utc_time,
+)
 from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.util import dt as dt_util
-
-from .const import ATTR_TRIGGER_ENTITY, ATTR_TRIGGER_STATE
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -15,22 +16,21 @@ _LOGGER = logging.getLogger(__name__)
 class HADailyCounterEntity(SensorEntity, RestoreEntity):
     """Sensor that increments when another entity hits a specific state."""
 
+    _attr_state_class = "total"
+    _attr_device_class = None
+    _attr_native_unit_of_measurement = None
+    _attr_icon = "mdi:counter"
+
     def __init__(self, hass: HomeAssistant, entry_id: str, counter_config: dict) -> None:
         self.hass = hass
         self._entry_id = entry_id
-        self._unique_id = f"{entry_id}_{counter_config['id']}"
-        self._name = counter_config["name"]
-        self._trigger_entity = counter_config["trigger_entity"]
-        self._trigger_state = counter_config["trigger_state"]
-        self._device_id = counter_config["id"]
-        self._device_name = counter_config["name"]
-        self._attr_native_value = 0
-
-        # 📈 Stats support + clean appearance
-        self._attr_state_class = "total"
-        self._attr_device_class = None
-        self._attr_native_unit_of_measurement = None
-        self._attr_icon = "mdi:counter"
+        self._unique_id: str = f"{entry_id}_{counter_config['id']}"
+        self._name: str = counter_config["name"]
+        self._trigger_entity: str = counter_config["trigger_entity"]
+        self._trigger_state: str = counter_config["trigger_state"]
+        self._device_id: str = counter_config["id"]
+        self._device_name: str = counter_config["name"]
+        self._attr_native_value: int = 0
 
     @property
     def unique_id(self) -> str:
@@ -59,7 +59,6 @@ class HADailyCounterEntity(SensorEntity, RestoreEntity):
             self._attr_native_value = int(last_state.state)
             _LOGGER.debug("Restored state for %s: %s", self._name, self._attr_native_value)
 
-        # Listen for state change
         self.async_on_remove(
             async_track_state_change(
                 self.hass,
@@ -68,14 +67,18 @@ class HADailyCounterEntity(SensorEntity, RestoreEntity):
             )
         )
 
-        # Schedule daily reset at midnight
         next_reset = self._get_next_reset_time()
         self.async_on_remove(
             async_track_point_in_utc_time(self.hass, self._reset_counter, next_reset)
         )
 
     @callback
-    def _handle_trigger_state_change(self, entity_id, old_state, new_state) -> None:
+    def _handle_trigger_state_change(
+        self,
+        entity_id: str,
+        old_state: State | None,
+        new_state: State | None,
+    ) -> None:
         """Handle a trigger state change."""
         if new_state and new_state.state == self._trigger_state:
             self._attr_native_value += 1
@@ -89,7 +92,6 @@ class HADailyCounterEntity(SensorEntity, RestoreEntity):
         self.async_write_ha_state()
         _LOGGER.debug("Counter '%s' reset to 0 at scheduled hour", self._name)
 
-        # Reschedule next reset
         next_reset = self._get_next_reset_time()
         async_track_point_in_utc_time(self.hass, self._reset_counter, next_reset)
 
